77a7e1d69d14a8cffbae948b3a49f0f8d5a9ffb6
KristyKostyor <ms.kostyor@gmail.com>
2023-05-01 18:18:30 +0300

    init

---
+++ task_2.js
@@ -0,0 +1,9 @@
+const newRandomNumberGenerator = (length, min, max, n, m) => {
+    const result = [];
+    for(let i=0; i < length; i++){
+        result.push(Math.floor(Math.random()) * (Math.abs(n-m)+1) + Math.min(n,m))
+    }
+    return result;
+}
+const randomNumberArr = newRandomNumberGenerator(10, 1,100, -100,100);
+console.log(randomNumberArr);
